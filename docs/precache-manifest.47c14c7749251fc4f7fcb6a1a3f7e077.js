2ff9e6ae76e9a084f7435c16ff036a4b72f3c156
fmizzell <fmizzell.dev@gmail.com>
2020-10-09 21:05:34 -0500

    First step.

---
+++ docs/precache-manifest.47c14c7749251fc4f7fcb6a1a3f7e077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67955a7d7fbe5e867a6657c32953deb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b907aad76174ecc68959",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f2038fb191d766d38dc",
+    "url": "/static/js/2.0cf51e81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0cf51e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b907aad76174ecc68959",
+    "url": "/static/js/main.a693f6ea.chunk.js"
+  },
+  {
+    "revision": "8dffe390a052c431377b",
+    "url": "/static/js/runtime-main.9c3b519b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
